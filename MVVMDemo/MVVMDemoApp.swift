86f6dcc555cc1be7ac7b7139bf69151e4cede3e7
Isaac <corralesruizi@gmail.com>
2024-09-11 21:02:25 -0700

    Initial Commit

---
+++ MVVMDemo/MVVMDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMDemoApp.swift
+//  MVVMDemo
+//
+//  Created by Isaac Corrales Ruiz on 11/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MVVMDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
